7fb32d7b26ef38342efc8d5c8135f10706e0eab1
Jorgen Bengtsson <jorgen@pm-production.nu>
2024-02-03 10:36:24 +0100

    Add FE React-Vite-Boilerplate

---
+++ frontend/src/components/app.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './../styles/app.scss';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="">FRONTEND</div>;
+}
+
+export default App;
